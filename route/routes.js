137b4f085eab37765ebb69a42d0f1b99c037ad4b
akiosannn <akiosannn@gmail.com>
2024-09-08 16:27:17 +0900

    Update your changes

--- route/routes.js
+++ route/routes.js
@@ -17,11 +17,10 @@ app.post("/Human",async (req,res) => {
     console.log("Request Body:",req.body); 
     const human = new Human({
         name: req.body.namae,
-        age: req.body.age || 0 
+        age: req.body.toshi || 0 
     });
     try{
         await human.save();
-        alert("登録が成功しました！！")
         res.redirect("/")
     } catch(err){
         res.status(500).send(err);
